3daebeebd265471f544a297193bb45bb20b1e475
viktor <alycasar@gmail.com>
2025-02-24 21:37:48 +0200

    debug

--- molytovnyk/molytovnyk/settings.py
+++ molytovnyk/molytovnyk/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&mq47ntz2-948*lpw^3=%ivobz#eo#tpf*zh+a1s3o4^^g@h3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['emanresu.pythonanywhere.com', '127.0.0.1']
 
